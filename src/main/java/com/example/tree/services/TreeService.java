832b10758e389864067d0f8c5d84d051a11e0c00
Ewelina <evelss005@gmail.com>
2020-03-18 17:40:42 +0100

    tree service methods

--- src/main/java/com/example/tree/services/TreeService.java
+++ src/main/java/com/example/tree/services/TreeService.java
@@ -1,4 +1,17 @@
 package com.example.tree.services;
 
+import com.example.tree.model.Person;
+import com.example.tree.model.Tree;
+
 public interface TreeService {
+
+    Tree createTree(Tree tree);
+
+    Tree getTreeById(Integer id);
+
+    Person selectMainPerson(Integer treeId, Integer personId);
+
+    Tree renameTree(Integer treeId, String newName);
+
+    boolean deleteTree(Integer id);
 }
