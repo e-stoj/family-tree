e3944f771f39cb2a7f7df72270a410f893f794d6
Ewelina <evelss005@gmail.com>
2020-03-25 17:16:03 +0100

    implementation tree service methods

--- src/main/java/com/example/tree/services/implementation/TreeServiceImpl.java
+++ src/main/java/com/example/tree/services/implementation/TreeServiceImpl.java
@@ -2,32 +2,50 @@ package com.example.tree.services.implementation;
 
 import com.example.tree.model.Person;
 import com.example.tree.model.Tree;
+import com.example.tree.repositories.PersonRepository;
+import com.example.tree.repositories.TreeRepository;
 import com.example.tree.services.TreeService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TreeServiceImpl implements TreeService {
 
+    @Autowired
+    TreeRepository treeRepository;
+
+    @Autowired
+    PersonRepository personRepository;
+
     @Override
     public Tree createTree(Tree tree) {
-        return null;
+        treeRepository.save(tree);
+        return tree;
     }
 
     @Override
     public Tree getTreeById(Integer id) {
-        return null;
+        return treeRepository.findById(id).orElseThrow(() -> new RuntimeException("Tree doesn't exist"));
     }
 
     @Override
     public Person selectMainPerson(Integer treeId, Integer personId) {
-        return null;
+        Tree tree = treeRepository.findById(treeId).orElseThrow(() -> new RuntimeException("Tree doesn't exist"));
+        Person person = personRepository.findById(personId).orElseThrow(() -> new RuntimeException("Person doesn't exist"));
+        tree.setMainPerson(person);
+        treeRepository.save(tree);
+        return person;
     }
 
     @Override
     public Tree renameTree(Integer treeId, String newName) {
-        return null;
+        Tree tree = treeRepository.findById(treeId).orElseThrow(() -> new RuntimeException("Tree doesn't exist"));
+        tree.setTreeName(newName);
+        treeRepository.save(tree);
+        return tree;
     }
 
     @Override
     public boolean deleteTree(Integer id) {
-        return false;
+        treeRepository.deleteById(id);
+        return true;
     }
 }
